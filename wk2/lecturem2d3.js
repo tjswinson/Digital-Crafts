20c132085053903656e9a0f7248b5fa319234638
tjswinson <tiffanyjswinson@gmail.com>
2021-10-02 09:57:00 -0400

    commit on main

---
+++ wk2/lecturem2d3.js
@@ -0,0 +1,20 @@
+let n = 1;
+while (n < 10) {
+    console.log(n);
+    n++;
+}
+console.log(n)
+// 10
+
+let m = 5;
+let conversation = '';
+while (m>0) {
+    console.log('one for me. ');
+    conversation += "one for me \m";
+    m--;
+    conversation += "one for you \m";
+    m--;
+}
+console.log(conversation);
+console.log(m);
+
