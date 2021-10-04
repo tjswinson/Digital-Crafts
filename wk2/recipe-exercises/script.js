c18a0854cf06d8d43c2e898d531044d716404ad6
tjswinson <tiffanyjswinson@gmail.com>
2021-10-04 17:25:31 -0400

    commit

--- wk2/recipe-exercises/script.js
+++ wk2/recipe-exercises/script.js
@@ -1,13 +1,15 @@
 function marinaraSauce(cookName) {
+    var two = 2;
+    var oneFourth = "1/4";
     console.log("Hey, " + cookName + "! This is my favorite marinara sauce recipe!" );
     console.log("- Heat a medium-large saucepan over medium heat.");
     console.log();
-    console.log("- Add 2 tspns of oil and 5 garlic cloves, cook until golden, about 2 minutes");
+    console.log("- Add " + two + " tspns of oil and 5 garlic cloves, cook until golden, about " + two + " minutes");
     console.log();
-    console.log("- Add 1/4 cup of water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("- Add " + oneFourth + " cup of water, " + two + " cans of crushed tomates, and salt and season with black pepper to taste.");
     console.log();
     console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
     console.log();
-    console.log("- Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.");
+    console.log("- Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
 }
 marinaraSauce("Adam");
